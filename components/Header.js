000ecbca14635bbf5d908dcd6327c0dd5db45a72
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-08-23 15:25:47 +0200

    add nav-styling and link

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <nav>Header</nav>;
+  return (
+    <nav>
+      <div className="navContainer">
+        <div className="left"></div>
+
+        <div className="logoContainer">Theory of Källsortering</div>
+
+        <div className="aboutContainer">
+          <Link href="about">
+            <a>Om oss</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
