61ae623b3cc4217825cb7916dab0a49710927f2a
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-08-23 11:20:03 +0200

    test-unity works

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Header from "../components/Header";
-
 import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game() {
   const { unityProvider } = useUnityContext({
     loaderUrl: "Build/SammanslagetHackaton(WebGL).loader.js",
-    dataUrl: "Build/SammanslagetHackaton(WebGL).data.gz",
-    frameworkUrl: "Build/SammanslagetHackaton(WebGL).framework.js.gz",
-    codeUrl: "Build/SammanslagetHackaton(WebGL).wasm.gz",
+    dataUrl: "Build/SammanslagetHackaton(WebGL).data",
+    frameworkUrl: "Build/SammanslagetHackaton(WebGL).framework.js",
+    codeUrl: "Build/SammanslagetHackaton(WebGL).wasm",
   });
 
   return <Unity unityProvider={unityProvider} />;
