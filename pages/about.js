8b82182b4f3900c8a6279a5d1a8b1eeb60d39130
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-08-24 13:34:00 +0200

    About-page almost done

--- pages/about.js
+++ pages/about.js
@@ -7,20 +7,40 @@ import Blue from "../public/frames/blue.svg";
 import BrownSecond from "../public/frames/brownSecond.svg";
 import BlueSecond from "../public/frames/blueSecond.svg";
 import Background from "../public/frames/background.svg";
+import Image from "next/image";
+import Header from "../components/Header";
 
 export default function About() {
   return (
-    <div className="description">
-      <Card name="Hanna Rosenberg" role="Webbutvecklare" image={Brown}></Card>
-      <Card name="Neo Lejondahl" role="Webbutvecklare" image={Pink}></Card>
-      <Card name="Emma Finnäle" role="Webbutvecklare" image={Orange}></Card>
-      <Card name="Teodor Söderberg" role="Webbutvecklare" image={Blue}></Card>
-      <Card
-        name="Victoria Amiri"
-        role="Webbutvecklare"
-        image={BrownSecond}
-      ></Card>
-      <Card name="Yrgo Göteborg" role="Lärdomsgatan" image={BlueSecond}></Card>
+    <div>
+      <Header></Header>
+      <div className="aboutContainer1">
+        <Image src={Background}></Image>
+        <div className="description">
+          <Card
+            name="Hanna Rosenberg"
+            role="Webbutvecklare"
+            image={Brown}
+          ></Card>
+          <Card name="Neo Lejondahl" role="Webbutvecklare" image={Pink}></Card>
+          <Card name="Emma Finnäle" role="Webbutvecklare" image={Orange}></Card>
+          <Card
+            name="Teodor Söderberg"
+            role="Webbutvecklare"
+            image={Blue}
+          ></Card>
+          <Card
+            name="Victoria Amiri"
+            role="Webbutvecklare"
+            image={BrownSecond}
+          ></Card>
+          <Card
+            name="Yrgo Göteborg"
+            role="Lärdomsgatan"
+            image={BlueSecond}
+          ></Card>
+        </div>
+      </div>
     </div>
   );
 }
